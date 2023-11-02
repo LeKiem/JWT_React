407ebfd709b613e1413ce35502eebe357ffae0b7
LeKiem <levankiemdev@gmail.com>
2023-11-02 08:02:14 +0700

    #9.1 Setup CSS - Cài Đặt SCSS Cho Dự Án React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
+        <p>Hello world React with Hoi Dan IT</p>
         <a
           className="App-link"
           href="https://reactjs.org"
