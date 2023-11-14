67a7269f6e1aefe4d6f8258ca24cbed8e74bf018
LeKiem <levankiemdev@gmail.com>
2023-11-14 21:15:24 +0700

    #9.3.3 Form Login Responsive - Tạo Form Cho Mobile/Laptop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,20 @@
 import "./Login.scss";
 const Login = (props) => {
   return (
-    <div className="login-container mt-4">
+    <div className="login-container">
       <div className="container">
-        <div className="row">
-          <div className="content-left col-7 ">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
             <div className="brand">JWT App</div>
-            <div className="tittle">Learn react with Eric</div>
+            <div className="tittle">
+              App giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của
+              bạn.
+            </div>
           </div>
-          <div className="content-right col-5  d-flex flex-column gap-3 py-3">
+
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none">JWT App</div>
+
             <input
               type="text"
               className="form-control"
@@ -20,7 +26,12 @@ const Login = (props) => {
               placeholder="Password"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center"> Forgot your password</span>
+            <span className="text-center">
+              {" "}
+              <a className="forgot-password" href="#">
+                Forgot your password
+              </a>
+            </span>
             <hr />
             <div className="text-center">
               <button className="btn btn-success"> Create new account</button>
