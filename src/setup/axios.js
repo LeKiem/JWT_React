096b497395c59a510142960fddcadb7213a0707b
Le Van Kiem <levankiemdev@gmail.com>
2024-01-27 23:52:04 +0700

    #11.6 JWT Middleware

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { toast } from "react-toastify";
 // Set config defaults when creating the instance
 const instance = axios.create({
   baseURL: "http://localhost:8080",
@@ -28,10 +28,53 @@ instance.interceptors.response.use(
     // Do something with response data
     return response.data;
   },
-  function (error) {
+  function (err) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+    // Do something with response err
+
+    const status = (err && err.response && err.response.status) || 500;
+
+    switch (status) {
+      // authentication (token related issues)
+      case 401: {
+        toast.error("Unauthorized the user. Please login...");
+        // window.location.href = "/login";
+        return Promise.reject(err);
+      }
+
+      // forbidden (permission related issues)
+      case 403: {
+        toast.error(`You don't permission to access this resource...`);
+
+        return Promise.reject(err);
+      }
+
+      // bad request
+      case 400: {
+        return Promise.reject(err);
+      }
+
+      // not found
+      case 404: {
+        return Promise.reject(err);
+      }
+
+      // conflict
+      case 409: {
+        return Promise.reject(err);
+      }
+
+      // unprocessable
+      case 422: {
+        return Promise.reject(err);
+      }
+
+      // generic api error (server related) unexpected
+      default: {
+        return Promise.reject(err);
+      }
+    }
+    // return Promise.reject(error);
   }
 );
 
